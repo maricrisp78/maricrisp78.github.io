cb835ee0785ee830c644e3e3ebd855884ded4095
marieashley_78 <marieashley_78@hotmail.com>
2016-03-21 00:16:08 -0400

    commit 3/20

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 jQuery(document).ready(function() {
 
 
-$('body').prepend(<a href="#" class="backtotop">Backt to top</a>);
-
+	$('body').prepend('<a href="#" class="backtotop">Back to top</a>');
+		
+			$(window).scroll(function(){
+				if ($(window).scrollTop() > 400){
+					$('a.backtotop').fadeIn('fast');
+					
+				} else {
+						$('a.backtotop').fadeOut('fast');
+				}
+			})
+				
+				$('a.backtotop').click(function(){
+				$('body')animate({
+					scrollTop: ()
+				},'fast');
+				
+				return false;
 			
-});
+			})
